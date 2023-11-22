fa5ac810bc4c5c541265babd0d57ec64987f9105
TiagoBarbosa88 <sombr43@gmail.com>
2023-11-22 20:37:36 -0300

    Exercicio e ARRAY-len

---
+++ array-len.py
@@ -0,0 +1,19 @@
+lista_musicos = [ 'Djavan', 'Roberto Carlos', 'Elis Regina', 'Tom Jobim', 'Milton Nascimento', 'Chico Buarque', 'Nara Leão', 'Pitty', 'Simonal', 'Moacir Santos', 'Caetano Veloso', 'Elza Soares', 'Paulinho da Viola', 'Yamandú Costa', 'Gal Costa'] 
+
+
+# quantidade de elementos do ARRAY lista_musicos
+qtde_lista_musicos = len(lista_musicos)
+print("Total de itens do Array lista_musicos :",qtde_lista_musicos)
+
+# índice 2
+indice_2 = lista_musicos[2]
+print("Indice -",indice_2)
+
+# indice 9
+indice_9 = lista_musicos[9]
+print("Indice -",indice_9)
+
+# indice 14
+indice_14 = lista_musicos[14]
+print("Indice -",indice_14)
+
