40fdece7750277dc382d61b3f28df8da8111a8a0
TiagoBarbosa88 <sombr43@gmail.com>
2023-11-22 20:23:47 -0300

    Exercicio e ARRAY

---
+++ array.py
@@ -0,0 +1,9 @@
+# Array com produtos de uma loja
+produtos_loja = ["Arroz", "Feijão", "Óleo", "Leite", "Carne"]
+
+# Array com anos de nascimento de familiares e amigos
+anos_nascimento = [1988, 1989, 2008, 2016]
+
+# Imprimindo os arrays
+print("Produtos da Loja:", produtos_loja)
+print("Anos de Nascimento:", anos_nascimento)
